8bad73f504891709f30d045cb26bf4373922066d
harit <harit@gmail.com>
2025-12-23 09:53:29 +0100

    initialize project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/inspire_environment/InspireEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.inspire_environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspireEnvironmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inspire_environment/InspireEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inspire_environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
